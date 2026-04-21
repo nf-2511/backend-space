156c77de08bbb8fd92ced7345ab4de5b4c789ade
bekzodmirzaaliyevdev <bekzod.mirzaaliyev@icloud.com>
2026-04-21 10:38:34 +0500

    AS

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+// settings
+app.use(cors())
+app.use(express.json())
+
+// database
+const database = [
+
+]
+
+// routes ( API )
+
+app.post('/register', (req, res) => {
+    const { role, id, password, name, surname, age, phone, gender } = req.body
+
+    if (!role || !id || !password) {
+        return res.status(400).json({ error: 'Missing required fields' })
+    }
+    // check if user already exists
+    const checkUser = database.find(user => user.id === id) // true / false
+
+    if (checkUser) {
+        // bor bo'lsa
+        return res.status(400).json({ error: 'User already exists' })
+    }
+
+    database.push({
+        role, id, password, name, surname, age, phone, gender, coin: 0, balance: 0, created_at: new Date().toLocaleString(), group: null, attendence: []
+    })
+
+    return res.status(200).json({ message: 'User created successfully' })
+})
+
+app.post('/login', (req, res) => {
+    const { id, password } = req.body
+
+    if (!id || !password) {
+        return res.status(400).json({ error: 'Missing required fields' })
+    }
+
+    const checkUser = database.find(user => user.id === id && user.password === password)
+
+    if (!checkUser) {
+        return res.status(400).json({ error: 'User not found' })
+    }
+
+    return res.status(200).json({ message: 'User logged in successfully' })
+})
+
+
+
+app.listen('3000', () => {
+    console.log('Server is running on port 3000')
+})
