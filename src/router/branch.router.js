1ae3fee2a5143764c824bb2cbad6987731da1857
bekzodmirzaaliyevdev <bekzod.mirzaaliyev@icloud.com>
2026-05-02 10:49:24 +0500

    ASD

---
+++ src/router/branch.router.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const branchModel = require('../models/branch.model')
+
+const router = express.Router()
+
+// API LIST
+
+// create branch
+router.post('/create', async (req, res) => {
+    const { name, address, phone, location, manager, tutors, mentors, students } = req.body
+
+    // validation
+    if (!name || !address || !phone || !location) {
+        return res.status(400).json({ error: 'Kerakli malumotlarni to`ldiring' })
+    }
+
+    try {
+        const newBranch = await branchModel.create({
+            name, address, phone, location, manager, tutors, mentors, students
+        })
+        res.status(200).json({ message: "Filial muvoffiqiyatli yaratildi", newBranch })
+    } catch (error) {
+        console.log(error)
+    }
+})
+// all branches
+router.get('/all', async (req, res) => {
+    try {
+        const branches = await branchModel.find()
+        res.status(200).json(branches)
+    } catch (error) {
+        console.log(error)
+    }
+})
+// branch edit by id
+
+
+// branch delete by id
+
+// http://localhost:3000/api/v1/branch/delete/038912038192083d
+router.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params
+    const deleteBranch = await branchModel.deleteOne({ _id: id })
+
+    res.status(200).json({ message: "Filial muvoffiqiyatli o'chirildi", deleteBranch })
+})
+// branch details by id
